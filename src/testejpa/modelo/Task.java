5c95547676ba69ed401696925a955417cd4e9a4a
36118482018.2@senacrjedu.local <36118482018.2@senacrjedu.local>
2019-06-08 16:27:57 -0300

    Primeiro commit configuracao jpa com hibernate

---
+++ src/testejpa/modelo/Task.java
@@ -0,0 +1,51 @@
+package testejpa.modelo;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+//@Table(name="tasks")
+public class Task {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String descricao;
+	private boolean finalizada;
+	
+	@Column(name="data_finalizacao", nullable=true)
+	@Temporal(TemporalType.DATE)
+	private Calendar dataFinalizacao;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public boolean isFinalizada() {
+		return finalizada;
+	}
+	public void setFinalizada(boolean finalizada) {
+		this.finalizada = finalizada;
+	}
+	public Calendar getDataFinalizacao() {
+		return dataFinalizacao;
+	}
+	public void setDataFinalizacao(Calendar dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
+}
